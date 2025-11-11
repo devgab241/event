130af812f4d2f36bc6fa4e9343dbda4c2926eda4
Brel7 <brelmassima1@gmail.com>
2025-11-11 14:00:13 +0100

    Mise à jour du footer et des métadonnées OG

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,7 +18,7 @@ const Footer = () => {
           <div>
             <div className="flex items-center space-x-2 mb-4">
               <img 
-                src="/logo.png" 
+                src="/images/logo.png" 
                 alt="EventiDecor" 
                 className="h-10 w-auto"
               />
